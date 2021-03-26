136098f681ee9a26a68063aae09f18834d88ba68
John <john@olivetoast.com>
2021-03-26 13:57:45 +0000

    Initial Commit

---
+++ SegmentedControl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SegmentedControl
+//
+//  Created by John on 26/03/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
