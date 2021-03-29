90188ff82c19cdc711796d0db009f37a2b0626fb
John <john@olivetoast.com>
2021-03-29 12:20:31 +0100

    Adds SegmentedControl.swift

---
+++ SegmentedControl/SegmentedControl.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControl.swift
+//  SegmentedControl
+//
+//  Created by John on 26/03/2021.
+//
+
+import Cocoa
+
+class SegmentedControl: NSControl {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
